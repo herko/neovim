4eb404b1b2ead7dcec53e9c9a1ceb52b4ee3a874
Lubomir Herko <lubomir@herko.sk>
2024-05-02 22:57:28 +0200

    Update neovim dashboard settings

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -5,15 +5,6 @@ return {
   lazy = true,
   opts = function()
     local dashboard = require("alpha.themes.dashboard")
---     local logo = [[
--- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
--- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
--- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
--- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
--- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
--- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
---     ]]
-
     local logo = [[
                                                     
              ████ ██████           █████      ██
@@ -28,11 +19,11 @@ return {
     dashboard.section.header.val = vim.split(logo, "\n")
     dashboard.section.buttons.val = {
       dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-      dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+      -- dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
       dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("s", " " .. "Restore Session", '<cmd>lua require("persistence").load()<cr>'),
-      dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/ <CR>"),
+      -- dashboard.button("s", " " .. "Restore Session", '<cmd>lua require("persistence").load()<cr>'),
+      -- dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/ <CR>"),
       dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
       dashboard.button("q", " " .. " Quit", ":qa<CR>"),
     }
